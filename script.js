1b0997773afe1f86667a3a86cd400bee2e5f43f1
Ana Beatriz <163312850+AnaBeatrizCorrea@users.noreply.github.com>
2026-05-29 12:46:04 -0300

    teste de sumir o placeholder

--- script.js
+++ script.js
@@ -21,32 +21,36 @@ function clique(){
     let Cupons=["BRASIL20", "CUPONZ10", "MPBE10", "ULTIMO05"];
     let Possui_Desconto;
     let Descontos=[0.2, 0.1, 0.1, 0.5];
-    let ValorDesc;
-    let Valor_Total;
-    let Valor_Total_Desc;
+    let ValorDesc = 0;
+    let Valor_Total=0;
+    let Valor_Destino=0;
+    let Valor_Total_Desc=0;
+    let Valor_Adicionais=0;
 
     for(let i = 0; i<= 5; i++){
         if(destino[i]){
-            let valorNumerico=destino[i];
+            Valor_Destino=destino[i];
             break; 
         }
     }
     
     for(let indC = 0; indC<= 3; indC++){
         if(adicionais[indC]){
-            let valorNumerico_Adicionais =adicionais[indC];           
+            Valor_Adicionais =adicionais[indC];           
         }
     }
 
+    Valor_Total = Valor_Destino + Valor_Adicionais; 
+    
     for(let indCC= 0; indCC <= 3; indCC++){
         if(Cupons[indCC]){
             Cupom_usado =Cupons[indCC];
             Possui_Desconto=Descontos[indCC];
-            ValorDesc=(Number(Possui_Desconto))*Valor_Total
+            ValorDesc = (Number(Possui_Desconto)) * Valor_Total;
             break;
         }
     }
-    
+    Valor_Total_Desc= 
     "Valor-Total"
     "Cupom-usado"
     "Valor-Desconto"         
